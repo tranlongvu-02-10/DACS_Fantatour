66c3f6a5872ab5e951007edad9dcf0152358d605
Tran Long Vu <tranlongvu02102004@gmail.com>
2025-05-04 13:44:35 +0700

    nhúng template

--- DACS_Fantatour/resources/views/clients/home.blade.php
+++ DACS_Fantatour/resources/views/clients/home.blade.php
@@ -13,26 +13,26 @@
     <!-- Title -->
     <title>Ravelo - Travel & Tour Booking HTML Template</title>
     <!-- Favicon Icon -->
-    <link rel="shortcut icon" href="assets/images/logos/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset('clients/assets/images/logos/favicon.png')}}" type="image/x-icon">
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet">
     
     <!-- Flaticon -->
-    <link rel="stylesheet" href="assets/css/flaticon.min.css">
+    <link rel="stylesheet" href="{{asset('clients/assets/css/flaticon.min.css')}}">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="assets/css/fontawesome-5.14.0.min.css">
+    <link rel="stylesheet" href="{{asset('clients/assets/css/fontawesome-5.14.0.min.css')}}">
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('clients/assets/css/bootstrap.min.css')}}">
     <!-- Magnific Popup -->
-    <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+    <link rel="stylesheet" href="{{asset('clients/assets/css/magnific-popup.min.css')}}">
     <!-- Nice Select -->
-    <link rel="stylesheet" href="assets/css/nice-select.min.css">
+    <link rel="stylesheet" href="{{asset('clients/assets/css/nice-select.min.css')}}">
     <!-- Animate -->
-    <link rel="stylesheet" href="assets/css/aos.css">
+    <link rel="stylesheet" href="{{asset('clients/assets/css/aos.css')}}">
     <!-- Slick -->
-    <link rel="stylesheet" href="assets/css/slick.min.css">
+    <link rel="stylesheet" href="{{asset('clients/assets/css/slick.min.css')}}">
     <!-- Main Style -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="{{asset('clients/assets/css/style.css')}}">
     
 </head>
 <body>
@@ -1042,27 +1042,27 @@
    
     
     <!-- Jquery -->
-    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="{{asset('clients/assets/js/jquery-3.6.0.min.js')}}"></script>
     <!-- Bootstrap -->
-    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="{{asset('clients/assets/js/bootstrap.min.js')}}"></script>
     <!-- Appear Js -->
-    <script src="assets/js/appear.min.js"></script>
+    <script src="{{asset('clients/assets/js/appear.min.js')}}"></script>
     <!-- Slick -->
-    <script src="assets/js/slick.min.js"></script>
+    <script src="{{asset('clients/assets/js/slick.min.js')}}"></script>
     <!-- Magnific Popup -->
-    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="{{asset('clients/assets/js/jquery.magnific-popup.min.js')}}"></script>
     <!-- Nice Select -->
-    <script src="assets/js/jquery.nice-select.min.js"></script>
+    <script src="{{asset('clients/assets/js/jquery.nice-select.min.js')}}"></script>
     <!-- Image Loader -->
-    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="{{asset('clients/assets/js/imagesloaded.pkgd.min.js')}}"></script>
     <!-- Skillbar -->
-    <script src="assets/js/skill.bars.jquery.min.js"></script>
+    <script src="{{asset('clients/assets/js/skill.bars.jquery.min.js')}}"></script>
     <!-- Isotope -->
-    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="{{asset('clients/assets/js/isotope.pkgd.min.js')}}"></script>
     <!--  AOS Animation -->
-    <script src="assets/js/aos.js"></script>
+    <script src="{{asset('clients/assets/js/aos.js')}}"></script>
     <!-- Custom script -->
-    <script src="assets/js/script.js"></script>
+    <script src="{{asset('clients/assets/js/script.js')}}"></script>
 
 </body>
 
