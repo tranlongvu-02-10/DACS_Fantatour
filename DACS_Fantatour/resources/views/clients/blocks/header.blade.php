94a8eb27a1206da8cb953b3143e6c928819cb9a9
Tran Long Vu <tranlongvu02102004@gmail.com>
2025-05-04 15:00:58 +0700

    Update

---
+++ DACS_Fantatour/resources/views/clients/blocks/header.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<!-- Mirrored from webtendtheme.net/html/2024/ravelo/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 07 Oct 2024 09:26:27 GMT -->
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title -->
+    <title>Ravelo - Travel & Tour Booking HTML Template</title>
+    <!-- Favicon Icon -->
+    <link rel="shortcut icon" href="{{asset('clients/assets/images/logos/favicon.png')}}" type="image/x-icon">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet">
+    
+    <!-- Flaticon -->
+    <link rel="stylesheet" href="{{asset('clients/assets/css/flaticon.min.css')}}">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('clients/assets/css/fontawesome-5.14.0.min.css')}}">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{asset('clients/assets/css/bootstrap.min.css')}}">
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="{{asset('clients/assets/css/magnific-popup.min.css')}}">
+    <!-- Nice Select -->
+    <link rel="stylesheet" href="{{asset('clients/assets/css/nice-select.min.css')}}">
+    <!-- Animate -->
+    <link rel="stylesheet" href="{{asset('clients/assets/css/aos.css')}}">
+    <!-- Slick -->
+    <link rel="stylesheet" href="{{asset('clients/assets/css/slick.min.css')}}">
+    <!-- Main Style -->
+    <link rel="stylesheet" href="{{asset('clients/assets/css/style.css')}}">
+    
+</head>
+<body>
