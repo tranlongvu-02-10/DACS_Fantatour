a2533b772138d53d5085dd1d145fd51b4e442ba4
Tran Long Vu <tranlongvu02102004@gmail.com>
2025-05-06 11:05:58 +0700

    Update

--- DACS_Fantatour/resources/views/clients/blocks/header.blade.php
+++ DACS_Fantatour/resources/views/clients/blocks/header.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="zxx">
 
-<!-- Mirrored from webtendtheme.net/html/2024/ravelo/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 07 Oct 2024 09:26:27 GMT -->
+<!-- Mirrored from webtendtheme.net/html/2024/ravelo/404.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 07 Oct 2024 09:28:37 GMT -->
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -9,123 +9,114 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Title -->
-    <title>Ravelo - Travel & Tour Booking HTML Template</title>
-    <!-- Favicon Icon -->
-    <link rel="shortcut icon" href="{{asset('clients/assets/images/logos/favicon.png')}}" type="image/x-icon">
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet">
+     <!-- Title -->
+     <title>Ravelo - Travel & Tour Booking HTML Template</title>
+     <!-- Favicon Icon -->
+     <link rel="shortcut icon" href="{{asset('clients/assets/images/logos/favicon.png')}}" type="image/x-icon">
+     <!-- Google Fonts -->
+     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet">
+     
+     <!-- Flaticon -->
+     <link rel="stylesheet" href="{{asset('clients/assets/css/flaticon.min.css')}}">
+     <!-- Font Awesome -->
+     <link rel="stylesheet" href="{{asset('clients/assets/css/fontawesome-5.14.0.min.css')}}">
+     <!-- Bootstrap -->
+     <link rel="stylesheet" href="{{asset('clients/assets/css/bootstrap.min.css')}}">
+     <!-- Magnific Popup -->
+     <link rel="stylesheet" href="{{asset('clients/assets/css/magnific-popup.min.css')}}">
+     <!-- Nice Select -->
+     <link rel="stylesheet" href="{{asset('clients/assets/css/nice-select.min.css')}}">
+     <!-- Animate -->
+     <link rel="stylesheet" href="{{asset('clients/assets/css/aos.css')}}">
+     <!-- Slick -->
+     <link rel="stylesheet" href="{{asset('clients/assets/css/slick.min.css')}}">
+     <!-- Main Style -->
+     <link rel="stylesheet" href="{{asset('clients/assets/css/style.css')}}">
     
-    <!-- Flaticon -->
-    <link rel="stylesheet" href="{{asset('clients/assets/css/flaticon.min.css')}}">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{asset('clients/assets/css/fontawesome-5.14.0.min.css')}}">
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="{{asset('clients/assets/css/bootstrap.min.css')}}">
-    <!-- Magnific Popup -->
-    <link rel="stylesheet" href="{{asset('clients/assets/css/magnific-popup.min.css')}}">
-    <!-- Nice Select -->
-    <link rel="stylesheet" href="{{asset('clients/assets/css/nice-select.min.css')}}">
-    <!-- Animate -->
-    <link rel="stylesheet" href="{{asset('clients/assets/css/aos.css')}}">
-    <!-- Slick -->
-    <link rel="stylesheet" href="{{asset('clients/assets/css/slick.min.css')}}">
-    <!-- Main Style -->
-    <link rel="stylesheet" href="{{asset('clients/assets/css/style.css')}}">
-    
-</head><div class="page-wrapper">
+</head>
+<body>
+    <div class="page-wrapper">
 
-    <!-- Preloader -->
-    <div class="preloader"><div class="custom-loader"></div></div>
+        <!-- Preloader -->
+        <div class="preloader"><div class="custom-loader"></div></div>
 
-    <!-- main header -->
-    <header class="main-header header-one white-menu menu-absolute">
-        <!--Header-Upper-->
-        <div class="header-upper py-30 rpy-0">
-            <div class="container-fluid clearfix">
+        <!-- main header -->
+        <header class="main-header header-one">
+            <!--Header-Upper-->
+            <div class="header-upper bg-white py-30 rpy-0">
+                <div class="container-fluid clearfix">
 
-                <div class="header-inner rel d-flex align-items-center">
-                    <div class="logo-outer">
-                        <div class="logo"><a href="index.html"><img src="assets/images/logos/logo.png" alt="Logo" title="Logo"></a></div>
-                    </div>
+                    <div class="header-inner rel d-flex align-items-center">
+                        <div class="logo-outer">
+                            <div class="logo"><a href="index.html"><img src="assets/images/logos/logo-two.png" alt="Logo" title="Logo"></a></div>
+                        </div>
 
-                    <div class="nav-outer mx-lg-auto ps-xxl-5 clearfix">
-                        <!-- Main Menu -->
-                        <nav class="main-menu navbar-expand-lg">
-                            <div class="navbar-header">
-                               <div class="mobile-logo">
-                                   <a href="index.html">
-                                        <img src="assets/images/logos/logo.png" alt="Logo" title="Logo">
-                                   </a>
-                               </div>
-                               
-                                <!-- Toggle Button -->
-                                <button type="button" class="navbar-toggle" data-bs-toggle="collapse" data-bs-target=".navbar-collapse">
+                        <div class="nav-outer mx-lg-auto ps-xxl-5 clearfix">
+                            <!-- Main Menu -->
+                            <nav class="main-menu navbar-expand-lg">
+                                <div class="navbar-header">
+                                   <div class="mobile-logo">
+                                       <a href="index.html">
+                                            <img src="assets/images/logos/logo-two.png" alt="Logo" title="Logo">
+                                       </a>
+                                   </div>
+                                   
+                                    <!-- Toggle Button -->
+                                    <button type="button" class="navbar-toggle" data-bs-toggle="collapse" data-bs-target=".navbar-collapse">
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                    </button>
+                                </div>
+
+                                <div class="navbar-collapse collapse clearfix">
+                                    <ul class="navigation clearfix">
+                                        <li class=" current"><a href="#">Home</a></li>
+                                        <li><a href="{{ route('about') }}">About</a></li>
+                                        <li class="dropdown"><a href="#">Tours</a>
+                                            <ul>
+                                                <li><a href="tour-sidebar.html">Tour Sidebar</a></li>
+                                                <li><a href="tour-guide.html">Tour Guide</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Destinations</a></li>
+                                        <li class="dropdown"><a href="#">Pages</a>
+                                            <ul>
+                                                <li><a href="faqs.html">faqs</a></li>
+                                                <li class="dropdown"><a href="#">Gallery</a></li>
+                                                <li><a href="contact.html">Contact Us</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown"><a href="#">blog</a>
+                                            <ul>
+                                                <li><a href="blog.html">blog List</a></li>
+                                                <li><a href="blog-details.html">blog details</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                            </nav>
+                            <!-- Main Menu End-->
+                        </div>
+                        
+                        <!-- Menu Button -->
+                        <div class="menu-btns py-10">
+                            <a href="contact.html" class="theme-btn style-two bgc-secondary">
+                                <span data-hover="Book Now">Book Now</span>
+                                <i class="fal fa-arrow-right"></i>
+                            </a>
+                            <!-- menu sidbar -->
+                            <div class="menu-sidebar">
+                                <button class="bg-transparent">
                                     <span class="icon-bar"></span>
                                     <span class="icon-bar"></span>
                                     <span class="icon-bar"></span>
                                 </button>
                             </div>
-
-                            <div class="navbar-collapse collapse clearfix">
-                                <ul class="navigation clearfix">
-                                    <li class=" current"><a href="#">Home</a></li>
-                                    <li><a href="{{ route('about') }}">About</a></li>
-                                    <li class="dropdown"><a href="#">Tours</a>
-                                        <ul>
-                                            <li><a href="tour-sidebar.html">Tour Sidebar</a></li>
-                                            <li><a href="tour-guide.html">Tour Guide</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="#">Destinations</a></li>
-                                    <li class="dropdown"><a href="#">Pages</a>
-                                        <ul>
-                                            <li><a href="faqs.html">faqs</a></li>
-                                            <li class="dropdown"><a href="#">Gallery</a></li>
-                                            <li><a href="contact.html">Contact Us</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="dropdown"><a href="#">blog</a>
-                                        <ul>
-                                            <li><a href="blog.html">blog List</a></li>
-                                            <li><a href="blog-details.html">blog details</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </div>
-
-                        </nav>
-                        <!-- Main Menu End-->
-                    </div>
-                    
-                    <!-- Nav Search -->
-                    <div class="nav-search">
-                        <button class="far fa-search"></button>
-                        <form action="#" class="hide">
-                            <input type="text" placeholder="Search" class="searchbox" required="">
-                            <button type="submit" class="searchbutton far fa-search"></button>
-                        </form>
-                    </div>
-                    
-                    <!-- Menu Button -->
-                    <div class="menu-btns py-10">
-                        <a href="contact.html" class="theme-btn style-two bgc-secondary">
-                            <span data-hover="Book Now">Book Now</span>
-                            <i class="fal fa-arrow-right"></i>
-                        </a>
-                        <!-- menu sidbar -->
-                        <div class="menu-sidebar">
-                            <button class="bg-transparent">
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <!--End Header Upper-->
-    </header>
-   
-
+            <!--End Header Upper-->
+        </header>
